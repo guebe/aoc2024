4f3564804ff8190ab41eaeb8aa68a8dc0726c360
gue <gue@debian.home>
2024-12-03 14:01:48 +0100

    improved oneliner

--- day3/oneliner.sh
+++ day3/oneliner.sh
@@ -1,1 +1,1 @@
-grep -oE "mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)" input | awk -f x.awk  | sed 's/mul(//' | sed 's/)//' | sed 's/,/*/' | tr '\n' "+" | sed 's/.$/\n/' | bc
+grep -oE "mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)" input | awk '/do\(/{dont=0};/don/{dont=1}/mul/&&!dont' | tr -d 'mul()' | tr ',\n' '*+' | sed 's/.$/\n/' | bc
