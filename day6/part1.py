0756205b27365f33365a9f4c0c64f25239a06d78
gue <gue@debian.home>
2024-12-06 18:07:30 +0100

    cleanup part1

--- day6/part1.py
+++ day6/part1.py
@@ -1,13 +1,13 @@
-a = open(0).read().splitlines()
+grid = open(0).read().splitlines()
 
-ROW = len(a)
-COL = len(a[0])
+ROW = len(grid)
+COL = len(grid[0])
 
 # determine initial position
 pos = 0
 for r in range(ROW):
     for c in range(COL):
-        if "^" == a[r][c]:
+        if "^" == grid[r][c]:
             pos = (r, c)
 
 def get_delta(direction):
@@ -30,7 +30,7 @@ def turn_right(direction):
     elif direction == 'v':
         return '<'
 
-direction = a[pos[0]][pos[1]] # initial direction
+direction = grid[pos[0]][pos[1]] # initial direction
 visited = set() # store visited positions
 
 while(True):
@@ -38,7 +38,7 @@ while(True):
     new = (pos[0]+delta[0],pos[1]+delta[1])
     if new[0] >= ROW or new[1] >= COL or new[0] < 0 or new[1] < 0:
         break # left grid
-    elif "#" == a[new[0]][new[1]]:
+    elif "#" == grid[new[0]][new[1]]:
         direction = turn_right(direction) # turn
     else:
         pos = new # move in current direction
