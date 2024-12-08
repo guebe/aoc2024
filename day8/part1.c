639db76193c1e814465ff242b7083b816f8f37a5
gue <gue@debian.home>
2024-12-08 12:00:53 +0100

    part2 C cleanup

--- day8/part1.c
+++ day8/part1.c
@@ -8,7 +8,7 @@ static char antinode[SZ][SZ];
 static inline int in_bounds(struct point p) { return p.x >= 0 && p.y >= 0 && p.x < SZ && p.y < SZ; }
 static inline struct point sub(struct point p1, struct point p2) { return (struct point){.x = p1.x - p2.x, .y = p1.y - p2.y }; }
 static inline struct point add(struct point p1, struct point p2) { return (struct point){.x = p1.x + p2.x, .y = p1.y + p2.y }; }
-static inline int is_valid(struct point p) { return (in_bounds(p) && antinode[p.y][p.x] != '#') ? (antinode[p.y][p.x] = '#', 1) : 0; }
+static inline int is_antinode(struct point p) { return (antinode[p.y][p.x] != '#') ? (antinode[p.y][p.x] = '#', 1) : 0; }
 int main(void) {
 	int result = 0;
 	for (char c = '0'; c <= 'z'; c++) { /* all possible frequencies */
@@ -18,7 +18,8 @@ int main(void) {
 		}
 		for (int i = 0; i < cnt-1; i++) for (int j = i+1; j < cnt; j++) {
 			struct point a1 = antenna[i], a2 = antenna[j], delta = sub(a1, a2); /* calc delta for all antenna pairs */
-			result += is_valid(add(a1, delta)) + is_valid(sub(a2, delta)); /* increase result by valid antinodes */
+			if (in_bounds(a1 = add(a1, delta))) result += is_antinode(a1); /* add antinode if not oob */
+			if (in_bounds(a2 = sub(a2, delta))) result += is_antinode(a2); /* sub antinode if not oob */
 	       	}
        	}
 	printf("%d\n", result); return 0;
