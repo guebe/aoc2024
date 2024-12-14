712722d7ca69c2d5c6959a8b58d8e52def64170a
gue <gue@debian.home>
2024-12-14 07:31:44 +0100

    cleanup part1

--- day14/part1.py
+++ day14/part1.py
@@ -1,9 +1,7 @@
 lines = open(0).read().splitlines()
 
-# FIXME
 #rows = 7 # FIXME
 #cols = 11 # FIXME
-# FIXME
 rows = 103 # FIXME
 cols = 101 # FIXME
 
@@ -14,22 +12,12 @@ def move(p, v):
     dx, dy = v
     nx = x + dx
     ny = y + dy
-    if nx >= cols:
-        nx = nx%cols
-    elif nx < 0:
-        nx = cols+nx
-    if ny >= rows:
-        ny = ny%rows
-    elif ny < 0:
-        ny = rows+ny
-    return (nx, ny)
-
+    return (nx%cols, ny%rows)
 
 for line in lines:
     p_line, v_line = line.strip('\n').split(' ')
     p = tuple(map(int,p_line.split('=')[1].split(',')))
     v = tuple(map(int,v_line.split('=')[1].split(',')))
-    #print(f"{p} {v}")
 
     for i in range(100):
         p = move(p, v)
@@ -46,18 +34,7 @@ for line in lines:
             q3+=1
         if x > hx and y > hy:
             q4+=1
-    print(f"{p}")
+    #print(f"{p}")
 
 print(q1*q2*q3*q4)
-
-#np = move((2,4), (2,-3))
-#print(f"{np}")
-#np = move(np, (2,-3))
-#print(f"{np}")
-#np = move(np, (2,-3))
-#print(f"{np}")
-#np = move(np, (2,-3))
-#print(f"{np}")
-#np = move(np, (2,-3))
-#print(f"{np}")
 
