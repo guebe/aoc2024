a61fc935d580f3aa717f24a114b04cb4203eac1b
guebe <57858743+guebe@users.noreply.github.com>
2024-12-21 20:55:48 +0100

    part2 try1

---
+++ day15/part2.py
@@ -0,0 +1,99 @@
+
+lines = open(0).read()
+
+field, dirs = lines.split('\n\n')
+field = field.split('\n')
+dirs = ''.join(dirs.strip('\n').split('\n'))
+
+boxes = set()
+walls = set()
+robot = None
+cols = len(field[0].strip('\n'))*2
+rows = len(field)
+
+for y,line in enumerate(field):
+    for x,char in enumerate(line.strip('\n')):
+        if char == '.':
+            pass
+        elif char == 'O':
+            boxes.add((x*2,y))
+        elif char == '#':
+            walls.add((x*2,y))
+        elif char == '@':
+            assert robot == None
+            robot=(x*2,y)
+        else:
+            assert False
+
+#print(field)
+#print(dirs)
+#print(robot)
+#print(walls)
+#print(boxes)
+
+directions = { '^': (0,-1), 'v': (0,1), '>': (1,0), '<': (-1,0) }
+
+def dbg():
+    for y in range(rows):
+        for x in range(cols):
+            if (x,y) in walls:
+                print('#', end='')
+            elif (x-1,y) in walls:
+                print('#', end='')
+            elif (x,y) == robot:
+                print('@', end='')
+            elif (x,y) in boxes:
+                print('[', end='')
+            elif (x-1,y) in boxes:
+                print(']', end='')
+            else:
+                print('.', end='')
+        print()
+    print()
+
+print("Initial state:")
+dbg()
+
+for d in dirs:
+    assert d in directions
+    dx,dy = directions[d]
+
+    todo = []
+    np = robot
+    do_move = False
+    while True:
+        np = (np[0]+dx,np[1]+dy)
+        if np in walls or (np[0]+dx,np[1]) in walls:
+            do_move = False
+            break
+        elif np in boxes in boxes:
+            todo.append(np)
+        elif (np[0]+dx,np[1]) in boxes:
+            todo.append((np[0]+dx,np[1]))
+        else: # empty space
+            do_move = True
+            break
+
+    if do_move:
+        robot = (robot[0]+dx,robot[1]+dy)
+        for box in todo:
+            boxes.remove(box)
+        for box in todo:
+            boxes.add((box[0]+dx,box[1]+dy))
+
+    print(f"Move {d}:")
+    dbg()
+
+print("FINISH")
+dbg()
+total = 0
+for x,y in boxes:
+    total += (y*100 + x)
+
+print(total)
+
+
+
+
+
+    
